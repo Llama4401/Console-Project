0d1f7d0fef88768ea8ac9aa1d3074a92ab3a29e7
Llama4401 <82985835+Llama4401@users.noreply.github.com>
2021-09-05 17:41:11 -0700

    Add files via upload
    this is cool project

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("Its my first console project");
+var name1 = "Henria";
+var name2 = "Hector";
+
+console.log(name1);
+console.log(name2);
+console.log(name1.concat(name2));
+
+console.log(name1.length);
+console.log(name2.length);
+
+fullname = name1.concat(name2);
+console.log(fullname.length);
+
+string1 = "Henria and Hector and best friends! ";
+console.log(string1);
+string2 = string1.replace("Henria","Airneh");
+console.log(string2);
+lower_string = string1.toLowerCase();
+console.log(lower_string);
+console.log(lower_string.toUpperCase());
+
+console.log("Math");
+console.log("94949499459596030391");
+console.log("94949499459596030390");
+console.log("Max Number");
+console.log(Math.max(94949499459596030391,94949499459596030390));
+console.log("Min Number");
+console.log(Math.min(94949499459596030391,94949499459596030390));
+console.log("4");
+console.log("Square root Number");
+
+console.log(Math.sqrt(4));
+
+console.log("6 raise to the power 3");
+console.log(Math.pow(6,3));
+
